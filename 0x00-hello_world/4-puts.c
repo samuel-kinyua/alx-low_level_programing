57b8507119d09749be29534d61ec713c2c192ed7
samuel-kinyua <samuelkinyua722@gmail.com>
2022-09-08 15:46:32 -0700

    created a puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main- Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("programming is like buliding a multilingual puzzle\n");
+	return (0);
+}
+
