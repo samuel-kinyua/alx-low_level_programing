a64a42247feea1964ba2463fadc891614ecea43a
samuel-kinyua <samuelkinyua722@gmail.com>
2022-09-10 14:09:18 -0700

    removed white space

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,24 +2,27 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main - Entry point
-*Return: Always 0 (succuss)
+ * main - Entry point
+ *Return: Always 0 (success)
 */
 int main(void)
-
 {
 	int n;
 
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
-
-	if (n > 1)
-		printf(" %i is positive \n", n);
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
 	else if (n < 0)
-		printf(" %i is negative\n", n);
-	else
-		printf(" %i is zero \n", n);
+	{
+		printf("%i is negative\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%i is zero\n", n);
+	}
 	return (0);
-
 }
