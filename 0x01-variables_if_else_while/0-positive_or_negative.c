d76554541ca3ea9fc1edcb9cb2c213747ae91cf1
samuel-kinyua <samuelkinyua722@gmail.com>
2022-09-10 11:10:34 -0700

    reorder the statements to + - 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,12 +14,12 @@ int main(void)
 
 	n = rand() - RAND_MAX / 2;
 
-	if (n < 0)
-		printf(" %d is negtive \n", n);
-	else if (n == 0)
-		printf(" %d is zero\n", n);
-	else if (n > 0)
+	if (n > 0)
 		printf(" %d is positive \n", n);
+	else if (n < 0)
+		printf(" %d is negative\n", n);
+	else if (n == 0)
+		printf(" %d is zero \n", n);
 
 	return (0);
 
