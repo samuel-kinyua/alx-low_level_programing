0fe471961eda4f00baf01902eef6a097f97f398c
samuel-kinyua <samuelkinyua722@gmail.com>
2022-09-11 23:28:46 -0700

    aded program tha prints all small letter

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point
+ *Return: always retur 0 to show it was successful
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+
+	{
+
+		putchar(alpha);
+
+		alpha++;
+
+	}
+
+	putchar('\n');
+
+
+
+	return (0);
+}
