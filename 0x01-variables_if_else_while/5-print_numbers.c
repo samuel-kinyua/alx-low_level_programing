4416b65d296a6f24b974311d1c120226c125eafa
samuel-kinyua <samuelkinyua722@gmail.com>
2022-09-12 02:56:40 -0700

    prrogram to print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point
+ * Return: Always 0 to show that it excecuted successfully
+ */
+
+int main(void)
+{
+	int numbers = 0;
+
+	while (numbers == 0)
+	{
+		printf("%d", numbers);
+			numbers++;
+	}
+	return (0);
+}
