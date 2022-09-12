d34b966603fe129d03ba41274af32b0dfedac60f
samuel-kinyua <samuelkinyua722@gmail.com>
2022-09-12 06:11:21 -0700

    program that prints alphabets from z

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point
+ *Return: Always 0 to show it executed successfullly
+ */
+
+
+int main(void)
+{
+	char lc = 'z';
+
+	while (lc >= 'a')
+	{
+		putchar(lc);
+		lc++;
+	putchar('\n');
+	}
+	return (0);
+}
