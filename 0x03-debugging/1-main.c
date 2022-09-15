8ca40160a3c5b807d8bd9709580660d42121d58b
samuel-kinyua <samuelkinyua722@gmail.com>
2022-09-15 14:59:20 -0700

    jfkdsm

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,8 +3,11 @@
 
 
 /**
+
 * main - causes an infinite loop
+
 * Return: 0
+
 */
 
 
@@ -12,17 +15,33 @@
 int main(void)
 
 {
-	int i;
 
-	printf("Infinite loop incoming :(\n");
-	i = 0;
+        int i;
 
-	/**
-	*	while (i < 1)
-	*	{
-	*	putchar(i);
-	* }
-	*/
-	printf("Infinite loop avoided! \\o/\n");
-	return (0);
+
+
+        printf("Infinite loop incoming :(\n");
+
+
+
+        i = 0;
+
+
+
+        /*while (i < 10)*/
+
+       /* {*/
+
+/*                putchar(i);*/
+
+ /*       }*/
+
+
+
+        printf("Infinite loop avoided! \\o/\n");
+
+
+
+        return (0);
+
 }
