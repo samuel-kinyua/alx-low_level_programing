1a705b750c592b14f7ff79e353a9ac9c521d30d7
samuel-kinyua <samuelkinyua722@gmail.com>
2022-09-16 16:02:35 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print nnumbers
+ * ecept 2 and 4
+ *
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		if ((i == 2) || (i == 4))
+		{
+			continue;
+		}
+		_putchar(i);
+	i++;
+	_putchar('\n');
+	}
+
+}
