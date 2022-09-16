0862389f1f60b7100361dd27c1aee60f79a55eb5
samuel-kinyua <samuelkinyua722@gmail.com>
2022-09-16 02:08:55 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -18,20 +18,19 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
+	i = 0;
 
+	/**
 
-	/*i = 0;*/
+	*while (i < 10)
 
+	*{
 
+	*putchar(i);
 
-	/*while (i < 10)*/
+	*}
 
-	/* {*/
-
-	/* putchar(i);*/
-
-	/*}*/
-
+	*/	
 
 
 	printf("Infinite loop avoided! \\o/\n");
