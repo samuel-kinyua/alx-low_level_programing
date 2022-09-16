cfed2393c52cb42a91e0ed1dada94f857a241f7f
samuel-kinyua <samuelkinyua722@gmail.com>
2022-09-16 15:22:29 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - mulitplys two integars
+ *
+ *@a: the first argument to be multiflied
+ *@b: the second arugments to be mulitpled
+ *
+ *Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
