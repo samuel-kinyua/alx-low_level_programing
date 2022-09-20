88dbb5baa9db3c1e7d63e5d87b5f878e1a0254d2
samuel-kinyua <samuelkinyua722@gmail.com>
2022-09-20 13:36:43 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_rev - prints srings in reverse
+ *
+ * @s: string to be reversed
+ *Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while (len--)
+	{
+		puchar(*(s + len));
+
+	putchar(10);
+	}
+}
