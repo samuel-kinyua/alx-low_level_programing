cdd1f869ce1a2f7617c7e0e505cd32e3316aa0fd
samuel-kinyua <samuelkinyua722@gmail.com>
2022-09-22 14:33:55 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@
 #include <string.h>
 
 char *_strcat(char *dest, char *src);i
-
+int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
 
 char *_strncpy(char *dest, char *src, int n);
