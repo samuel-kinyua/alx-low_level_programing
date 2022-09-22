2e0ebe7f5688c8948f785b5dcc2041f32528c98f
samuel-kinyua <samuelkinyua722@gmail.com>
2022-09-22 14:24:17 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,11 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/**
+ *the prototypes of this prograams
+ *
+ */
 #include <stdio.h>
 #include <string.h>
 
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src);i
 
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
+
+char *cap_string(char *);
 
 #endif /*MAIN_H*/
 
