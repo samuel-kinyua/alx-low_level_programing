e8c966780ceed4635503e52840cc1cc7d1b88401
samuel-kinyua <samuelkinyua722@gmail.com>
2022-09-28 07:25:32 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,18 +9,12 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
-
 	if (*s == '\0')
 	{
+		_putchar('\n');
 		return;
 	}
-	else
-	{
-		_putchar(*s);
-		_puts_recursion(++s);
-	}
+	_putchar(*s);
+	_puts_recursion(++s);
 
-	_putchar('\n');
 }
