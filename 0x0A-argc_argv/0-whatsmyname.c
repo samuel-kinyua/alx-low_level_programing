24e312364db54d277b8a81e62e61f6f003d04018
samuel-kinyua <samuelkinyua722@gmail.com>
2022-09-30 09:36:01 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,16 +9,8 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-
 	if (argc > 0)
-	{
-		for (i = 0; i != '\0'; i++)
-		{
-			(void) _putchar(*argv[i]);
-			_putchar('\n');
+		printf("%s\n", argv[0]);
 
-		}
-	}
 	return (0);
 }
