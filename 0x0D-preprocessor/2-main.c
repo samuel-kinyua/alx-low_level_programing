ca45a00af5217b1d02471b24bc7bc73f95ca7527
samuel-kinyua <samuelkinyua722@gmail.com>
2022-10-12 01:15:56 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  *main - prints the name of file where it is complie from
+  *
+  *Return: 0 0n success
+  */
+
+int  main(void)
+{
+	printf("file name is: %s\n", __FILE__);
+	return (0);
+}
