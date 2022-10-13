d5692ca2e8f01310e42d5b091e91697f1f0ccbbf
samuel-kinyua <samuelkinyua722@gmail.com>
2022-10-13 16:24:30 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,15 +15,17 @@ struct dog
 	char *owner;
 };
 
+/**
+  * dog_t - Typedef for struct dog
+  */
+
 typedef struct dog dog_t;
 
-#include <stdlib.h>
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
-/*dog_t *new_dog(char *name, float age, char *owner);*/
+dog_t *new_dog(char *name, float age, char *owner);
 
-/*void free_dog(dog_t *d);*/
-
+void free_dog(dog_t *d);
 #endif/*DOG_H*/
