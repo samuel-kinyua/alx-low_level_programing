e090602161ed940524577632bfbb897a0ba2019e
samuel-kinyua <samuelkinyua722@gmail.com>
2022-10-13 08:28:20 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,8 +10,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		retrn;
+	if (d)
+		return;
 
 	d->name = name;
 	d->age = age;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,7 @@ struct dog
 	char *owner;
 };
 
+#include <stdlib.h>
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif/*DOG_H*/
