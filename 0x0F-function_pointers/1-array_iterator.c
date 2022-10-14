90e285d4507d0b03eb3a92bbb567c01e85f936cc
samuel-kinyua <samuelkinyua722@gmail.com>
2022-10-14 14:40:10 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,8 +16,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t i;
 
 	if (array == NULL || size <= 0 || action == NULL)
-
+	{
 		return;
+	}
 
 	for (i = 0; i < size; i++)
 
