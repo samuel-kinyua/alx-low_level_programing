30a1ad460c573ac8ed3835b034a1f478f728de87
samuel-kinyua <samuelkinyua722@gmail.com>
2022-10-14 14:20:58 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,22 @@
 #include "function_pointers.h"
+#include <stddef.h>
+
 /**
   *print_name - function name
   *@name: parameter passed
   *@f: function
+  *
+  *Return: void pointer
   */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL || f != NULL)
-		f(name);
+	
+
+	if (name == NULL || f == NULL)
+
+		return;
+
+	f(name);
 
 }
