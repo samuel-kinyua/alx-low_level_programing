7da7e62acaa79ecaa32fb8bafa7a320a3cab0260
samuel-kinyua <samuelkinyua722@gmail.com>
2022-10-18 01:43:02 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+  *sum_them_all - adds all the parameters
+  *@n: first arguments
+  *
+  *Return: sum of int type
+  */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i;
+	va_list ap;
+
+	if (n == 0)
+		return (0);
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,17 @@
+0x10-variadic_functions
+
+#Tasks
+function that returns the sum of all its parameters.
+
+function that prints numbers, followed by a new line.
+
+function that prints strings, followed by a new line.
+
+function that prints anything.
+
+#Resources
+variadic function
+stdarg.h
+
+# My thoughts 
+this project is excent to practice varidic function, it also good to practice for the printf project.
